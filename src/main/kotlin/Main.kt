202c6fbd48961dd4748b97ca6bf706654a06af7e
Badr-1 <badrmohamedashraf@gmail.com>
2023-01-03 20:08:16 +0200

    🎨 Improved User Input and Validation

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,28 +1,58 @@
+import com.github.kinquirer.KInquirer
+import com.github.kinquirer.components.promptConfirm
+import com.github.kinquirer.components.promptList
+
 object Main {
     @JvmStatic
     fun main(args: Array<String>) {
         displayExample()
         while (true) {
-            println(white("Enter Empty Name To Exit."))
+
+            val format: String = KInquirer.promptList(
+                message = "What size do you need?",
+                choices = listOf(
+                    "Both", "Name Only"
+                ),
+                hint = "press Enter to pick",
+            )
+
             print(white("Enter name and surname: "))
-            val name = readln()
-            if (name.isEmpty()) {
+            val name: String
+            val nameFont: Font
+            val nameColor: String
+            "name".apply {
+                name = getText(this)
+                nameFont = getFontOf(this)
+                nameColor = getColor(this)
+            }
+
+            when (format) {
+                "Both" -> {
+                    print(white("Enter status: "))
+                    val status: String
+                    val statusFont: Font
+                    val statusColor: String
+                    "status".apply {
+                        status = getText(this)
+                        statusFont = getFontOf(this)
+                        statusColor = getColor(this)
+                    }
+
+                    val border = getBorder()
+                    display(nameFont, name, statusFont, status, nameColor, statusColor, border)
+                }
+
+                else -> {
+                    val border = getBorder()
+                    display(nameFont, name, nameColor = nameColor, border = border)
+                }
+            }
+
+
+            val quit: Boolean = KInquirer.promptConfirm(message = white("Quit ?"), default = false)
+            if (quit) {
                 break
             }
-            print(white("Enter status: "))
-            val status = readln()
-
-            val nameFont = getFontOf("name")
-            val nameColor = getColor()
-            var statusFont: Font = medium
-            var statusColor: String = WHITE
-            if (status.isNotEmpty()) {
-                statusFont = getFontOf("status")
-                statusColor = getColor()
-            }
-            val border = getBorder()
-
-            display(nameFont, name, statusFont, status, nameColor, statusColor, border)
         }
     }
 }
--- src/main/kotlin/Util.kt
+++ src/main/kotlin/Util.kt
@@ -1,86 +1,90 @@
 import com.github.ajalt.mordant.rendering.TextColors.*
 import com.github.ajalt.mordant.rendering.TextColors.Companion.rgb
+import com.github.kinquirer.KInquirer
+import com.github.kinquirer.components.ListViewOptions
+import com.github.kinquirer.components.promptInput
+import com.github.kinquirer.components.promptList
+
 import kotlin.math.roundToInt
 
 val roman = Font("roman.txt", 10)
 val medium = Font("medium.txt", 5)
 
 const val WHITE = "#ffffff"
+
+val fontsOptions = listOf("roman", "medium")
+
+fun getText(what: String): String {
+    do {
+        val text: String = KInquirer.promptInput(message = "Enter $what: ")
+        if (text.isNotEmpty()) {
+            return text
+        } else {
+            println(red("Field Required!"))
+        }
+
+    } while (true)
+}
+
 fun getFontOf(what: String): Font {
-    val font: Font
-    while (true) {
-        print(white("$what Font ((r) roman/ (m) medium) (Optional): "))
-        val choice = readln()
-        if (choice.isEmpty()) {
-            font = when (what) {
-                "name" -> roman
-                else -> medium
-            }
-            break
-        }
-        when (choice.first()) {
-            'm' -> {
-                font = medium
-                break
-            }
 
-            'r' -> {
-                font = roman
-                break
-            }
-
-            else -> {
-                println(red("Invalid choice!"))
-            }
-        }
+    val fontName = KInquirer.promptList(
+        message = "$what Font?",
+        choices = if (what == "name") fontsOptions else fontsOptions.reversed(),
+        hint = "press Enter to pick",
+        viewOptions = ListViewOptions(
+            questionMarkPrefix = "🎨",
+            cursor = " 🖌 ",
+            nonCursor = "    ",
+        )
+    )
+    val font: Font = when (fontName) {
+        "roman" -> roman
+        else -> medium
     }
+
     return font
 }
 
-fun getColor(): String {
-    var color: String
-    while (true) {
-        print(white("Color Hex (Optional): "))
-        color = readln()
+fun getColor(what: String): String {
+    do {
+        var color: String = KInquirer.promptInput(message = "$what Color Hex (Optional): ")
         if (color.isEmpty()) {
             return WHITE // default
         } else {
-            if (color.length == 3 || color.length == 6) {
+            color = color.removePrefix("#")
+            if (color.matches(Regex("^#?([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})\$"))) {
                 color = color[0].toString().repeat(2) + color[1].toString().repeat(2) + color[2].toString().repeat(2)
-                break
+                return color
             } else {
                 println(red("Invalid format!"))
             }
         }
-    }
-    return color
+    } while (true)
 }
 
 fun getBorder(): String {
-    var border: String
-    while (true) {
-        print(white("Border one character (Optional): "))
-        border = readln()
+
+    do {
+        val border: String = KInquirer.promptInput(message = "Border one character (Optional): ")
         if (border.isEmpty()) {
             return "8" // default
         } else {
             if (border.length == 1) {
-                break
+                return border
             } else {
                 println(red("Invalid format!"))
             }
         }
-    }
-    return border
+    } while (true)
 }
-
 
 
 fun display(
     nameFont: Font,
     name: String,
-    statusFont: Font,
-    status: String,
+    statusFont: Font = medium,
+    status: String = "",
     nameColor: String = WHITE,
     statusColor: String = WHITE,
     border: String = "8",
@@ -131,7 +135,7 @@ fun display(
                 }"
             )
         }
-        if (statusWithFont.width != 0) {
+        if (status.isNotEmpty()) {
             statusWithFont.value.forEach {
                 println("${white(border.repeat(2))}  ${rgb(statusColor)(it)}  ${white(border.repeat(2))}")
             }

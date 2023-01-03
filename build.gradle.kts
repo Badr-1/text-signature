bfa278debe8b4700c220090a665d90783540b730
Badr-1 <badrmohamedashraf@gmail.com>
2023-01-03 20:05:11 +0200

    ➕ For Better Display

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,9 +11,18 @@ version = "v1.0"
 repositories {
     mavenCentral()
 }
+allprojects {
+    repositories {
+        maven { url = uri("https://jitpack.io") }
+    }
+}
 
 dependencies {
-    implementation ("com.github.ajalt.mordant:mordant:2.0.0-beta9")
+    // https://mvnrepository.com/artifact/org.fusesource.jansi/jansi
+    implementation("org.fusesource.jansi:jansi:2.4.0")
+    implementation ("org.jline:jline:3.21.0")
+    implementation("com.github.ajalt.mordant:mordant:2.0.0-beta9")
+    implementation("com.github.kotlin-inquirer:kotlin-inquirer:0.1.0")
     testImplementation(kotlin("test"))
 }
 
